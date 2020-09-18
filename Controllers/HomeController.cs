f6c75fdfeda68faacd134ce8e2366dd55274935d
Siddharth Shinde <shinds3@mcmaster.ca>
2020-09-18 03:15:41 -0400

    Added Index View.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace PartyInvites.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello world";
+            return View();
         }
     }
 }
