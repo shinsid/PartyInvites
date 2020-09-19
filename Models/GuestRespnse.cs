0a47972cdf69123f3c2cb69ebab22de5212fbcf4
Siddharth Shinde <shinds3@mcmaster.ca>
2020-09-19 11:23:04 -0400

    Added GuestResponse Model class.

---
+++ Models/GuestRespnse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestRespnse
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool? WillAttend { get; set; }
+    }
+}
